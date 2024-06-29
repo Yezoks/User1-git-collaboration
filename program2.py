2c3e94ae6dbded2cb923a59449abb9eff687ccde
adeyemi-byte <adeyemiolayinka636@gmail.com>
2024-06-29 04:43:23 +0100

    Added 7th line from User2

--- program2.py
+++ program2.py
@@ -10,3 +10,5 @@ print ("Added 4th line from User2 local system")
 print ("Added 5th line from User2 local system")
 #-----------------------------------------------
 print ("Added 6th line from User2 local system")
+#-----------------------------------------------
+print ("Added 7th line from User2 local system")
