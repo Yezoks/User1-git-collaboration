0504d6af592ce86516b724a3ce1769e120b6b2fc
adeyemi-byte <adeyemiolayinka636@gmail.com>
2024-06-30 16:14:09 +0100

    Interface Config

--- program1.py
+++ program1.py
@@ -13,3 +13,10 @@ print ("Added 6th line from User2 local system")
 print ("Added 6th line from User2 local system")
 #-----------------------------------------------
 print ("Added 7th line from User2 local system")
+
+interface GigabitEthernet1/0/49
+ description PTP(IP-ACCESS-PTP-NNEKA-RESIDENCE-10.110.0.223)
+ switchport access vlan 21
+ switchport trunk allowed vlan 500,738
+ switchport mode trunk
+ load-interval 30
